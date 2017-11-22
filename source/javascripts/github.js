08bf1574b0452e13b5ae212fb9247dcdeddbcf18
Thibaud Colas <thibaudcolas@gmail.com>
2017-11-22 22:58:51 +0200

    Remove dependency on jQuery

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -1,42 +1,78 @@
-var github = (function(){
-  function escapeHtml(str) {
-    return $('<div/>').text(str).html();
-  }
-  function render(target, repos){
-    var i = 0, fragment = '', t = $(target)[0];
+// https://stackoverflow.com/a/22780569/1798491
+function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
 
-    for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
-    }
-    t.innerHTML = fragment;
-  }
-  return {
-    showRepos: function(options){
-      var elt = $(options.target);
-      var user = elt.attr('data-user');
-      var count = parseInt(elt.attr('data-count'), 10);
-      console.log(user, count);
+    var script = document.createElement('script');
+    script.async = true;
+    script.defer = true;
+    script.src =
+        url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
 
-      $.ajax({
-          url: "https://api.github.com/users/"+user+"/repos?sort=pushed&callback=?"
-        , dataType: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-        , success: function(data) {
-          var repos = [];
-          if (!data || !data.data) { return; }
-          for (var i = 0; i < data.data.length; i++) {
-            // Skip forks.
-            if (data.data[i].fork) { continue; }
-            repos.push(data.data[i]);
-          }
-          if (count) { repos.splice(count); }
-          render(options.target, repos);
+// https://stackoverflow.com/a/6234804/1798491
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
+
+var github = (function() {
+    function render(target, repos) {
+        var fragment = '';
+
+        for (var i = 0; i < repos.length; i++) {
+            fragment +=
+                '<li><a href="' +
+                repos[i].html_url +
+                '">' +
+                repos[i].name +
+                '</a><p>' +
+                escapeHtml(repos[i].description || '') +
+                '</p></li>';
         }
-      });
+        target.innerHTML = fragment;
     }
-  };
+
+    return {
+        showRepos: function(options) {
+            var container = document.querySelector(options.target);
+            var user = container.dataset.user;
+            var count = parseInt(container.dataset.count, 10);
+            console.log(user, count);
+
+            jsonp(
+                'https://api.github.com/users/' + user + '/repos?sort=pushed',
+                function(data) {
+                    var repos = [];
+                    if (!data || !data.data) {
+                        return;
+                    }
+                    for (var i = 0; i < data.data.length; i++) {
+                        // Skip forks.
+                        if (data.data[i].fork) {
+                            continue;
+                        }
+                        repos.push(data.data[i]);
+                    }
+                    if (count) {
+                        repos.splice(count);
+                    }
+                    render(container, repos);
+                },
+            );
+        },
+    };
 })();
 
 github.showRepos({
-    target: '#gh_repos'
+    target: '#gh_repos',
 });
